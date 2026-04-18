92c2a397fd2440c2bd98749aa152ec2f4466aa35
praveen-kumar0935 <praveenbonda0935@gmail.com>
2026-04-18 07:57:15 +0530

    Time: 3 ms (58.86%) | Memory: 20.4 MB (80.09%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        result = len(nums)
+        for i, num in enumerate(nums):
+            result ^= i ^ num
+        return result
+        
